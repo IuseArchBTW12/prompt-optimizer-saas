3981e23a2fedff0be3be2479678d7808d8182cc5
JoshuaC <balistictwitch@gmail.com>
2026-01-14 19:06:10 +1100

    fix: correct type definition for children prop in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.NodeNode;
+  children: React.ReactNode;
 }>) {
   return (
     <ClerkProvider
